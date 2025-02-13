cde800e4ae208d23c8629a91bac398799ec92b40
saurabh kumar <saurabhslite2@gmail.com>
2025-02-13 13:48:07 +0530

    Add javascript

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const checkboxList = document.querySelectorAll('.custom-checkbox')
+const checkboxList = document.querySelectorAll(".custom-checkbox");
+const inputFields = document.querySelectorAll(".goal-input");
+const progressBar = document.querySelector(".progress-bar");
+const progressValue = document.querySelector('.progress-value');
+const errorLabel = document.querySelector(".error-level");  
 
-checkboxList.forEach((chekbox)=>{
-  chekbox.addEventListener('click',(e)=>{
-    chekbox.parentElement.classList.toggle('completed')
-  })
-  
-})
+checkboxList.forEach((checkbox) => {
+  checkbox.addEventListener("click", (e) => {
+    const allGoalsAdded = [...inputFields].every((input) => {
+      return input.value.trim();  
+    });
+
+    if (allGoalsAdded) {
+      checkbox.parentElement.classList.toggle("completed");
+      const completedGoals = document.querySelectorAll('.completed').length;
+      progressValue.style.width = `${(completedGoals/3) * 100}%`;
+      progressValue.querySelector('span').textContent = `${completedGoals}/3 completed`;
+      errorLabel.style.display = 'none';  
+    } else {
+      progressBar.classList.add("show-error");
+      errorLabel.style.display = 'block';  
+    }
+  });
+});
+
+inputFields.forEach((input) => {
+  input.addEventListener("focus", () => {
+    progressBar.classList.remove("show-error");
+    errorLabel.style.display = 'none';  
+  });
+});
